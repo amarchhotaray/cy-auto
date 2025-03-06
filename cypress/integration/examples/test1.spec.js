bf55f3f21e27e58064db478c93af60aa39a04147
amarchhotaray <amarchhotaray@outlook.com>
2025-03-07 02:03:08 +0530

    test(master): commiting

--- cypress/integration/examples/test1.spec.js
+++ cypress/integration/examples/test1.spec.js
@@ -1,5 +1,54 @@
 describe('My First Test', () => {
     it('My First Test Case', () => {
-      expect(true).to.equal(true)
+      cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+      cy.get('input[type="search"]').type('ca')
+      cy.wait(2000)
+      cy.get('.product:visible').should('have.length', 4)
+
+      // Parent child chaining
+      cy.get('.products').find('.product').should('have.length', 4)
+
+      // cy.get(':nth-child(3) > .product-action > button').click() // This is not a good practice
+      cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+      // using each
+      cy.get('.products').find('.product').each(($ele, index, $list) => {
+        const textVeg = $ele.find('h4.product-name').text()
+        if(textVeg.includes('Cashews')) {
+          cy.wrap($ele).find('button').click()
+        }
+      })
+
+      cy.get('.brand').then((logoElement) => {
+        cy.log(logoElement.text())
+      })
+
+      // assert if logo text is correctly displayed
+      cy.get('.brand').should('have.text', 'GREENKART')
+
+      // alias
+      cy.get('.products').as('productLocator')
+      cy.get('@productLocator').find('.product').eq(1).contains('ADD TO CART').click()
+    })
+
+    it('My Second Test Case', () => {
+      cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+      cy.get('input[type="search"]').type('ca')
+      cy.wait(2000)
+
+      cy.get('.products').as('productLocator')
+
+      cy.get('@productLocator').find('.product').each(($ele, index, $list) => {
+        const textVeg = $ele.find('h4.product-name').text()
+        if(textVeg.includes('Cashews')) {
+          cy.wrap($ele).find('button').click()
+        }
+      })
+
+      // cy.get('@productLocator').find('.product').eq(1).contains('ADD TO CART').click()
+
+      cy.get('.cart-icon > img').click()
+      cy.contains('PROCEED TO CHECKOUT').click()
+      cy.contains('Place Order').click()
     })
 })
---
+++ cypress/plugins/index.js
@@ -0,0 +1,15 @@
+// plugins/index.js can be used to load plugins
+
+// You can change the location of plugins file or turn off loading plugins using pluginsFile configuration option
+
+// This function is called when a project is opened or re-opened (e.g. due to the project's config changing)
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+//   on('task', {
+//     log(message) {
+//       console.log(message)
+//       return null
+//     }
+//   })
+}
